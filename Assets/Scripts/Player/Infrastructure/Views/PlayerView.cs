78ec4eec61820ff1202f6a6b55336ba8e0112d54
CptSoap <cowbear6598@gmail.com>
2024-06-28 00:09:13 +0800

    :construction: temporary remove network identity for complete game main logic for player

--- Assets/Scripts/Player/Infrastructure/Views/PlayerView.cs
+++ Assets/Scripts/Player/Infrastructure/Views/PlayerView.cs
@@ -1,9 +1,8 @@
-﻿using Mirror;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Player.Infrastructure.Views
 {
-	public class PlayerView : NetworkBehaviour
+	public class PlayerView : MonoBehaviour
 	{
 		public void Move(Vector3 move)
 			=> transform.position += move;
